e9f7bfe3e306f205e07bd58991fa6f739327f9ed
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-08-06 01:29:01 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,18 +1,7 @@
 require_relative "./vehicle.rb"
 
-class Vehicle
-  attr_accessor :wheel_size, :wheel_number
-
-  def initialize(wheel_size, wheel_number)
-    @wheel_size = wheel_size
-    @wheel_number = wheel_number
-  end
-
+class Car<Vehicle
   def go
-    return "vrrrrrrrooom!"
-  end
-
-  def fill_up_tank
-    return "filling up!"
+    return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end
 end
